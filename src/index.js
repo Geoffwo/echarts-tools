0c51846b68747343640ab0b035c4b37d0f7ab03a
wgx <2997605851@qq.com>
2025-07-14 14:33:14 +0800

    fix:修复内置策略注册异常

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@
 * Copyright (c) 2025 geoffwo
 * This source code is licensed under the ISC license
 */
+import './strategy/xIndex.js'; // 注册内置策略
 export { createBasicChartInstance, drawBasicChartFactory, printDoc, importEcharts } from './utils/echartsUtils.js';
 export { strategyUtils, registerEventHandler,registerSimpleEventHandler } from './utils/strategyUtils.js';
